bb66d9bbc75f5f70aacd2a8efc124ac912c1345f
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-10-19 21:37:01 +0800

    Add files via upload

--- src/day1/Hello6.java
+++ src/day1/Hello6.java
@@ -1,6 +1,6 @@
 package day1;
 
-public class Hello5 {
+public class Hello6 {
 
 	public static void main(String[] args) {
 		double r = 345;
@@ -8,12 +8,11 @@ public class Hello5 {
 		double pi = Math.PI;
 		System.out.println(pi);
 		// 求圓面積與球體積各是多少 ?
-		double area = r * r * pi;
-		double volumn = 4.0 / 3 * (r * r * r * pi);
-		// 格式化整數 %d, 浮點數 %f
+		double area = Math.pow(r, 2) * pi;
+		double volumn = 4.0 / 3 * Math.pow(r, 3) * pi;
 		System.out.printf("半徑: %f 圓面積: %f 球體積: %f\n", r, area, volumn);
 		System.out.printf("半徑: %,.1f 圓面積: %,.1f 球體積: %,.1f\n", r, area, volumn);
-		
+	
 	}
 
 }
